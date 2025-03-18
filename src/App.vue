9694a804c2a22354407dfb9a991d2d1970b3925f
Jatin-Kamboj <er.jatinkamboj96@gmail.com>
2025-03-18 13:53:18 +0530

    Added applayout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,8 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-import { AppFooter, AppHeader } from "@/layouts/AppLayout";
+import { AppLayout } from "@/layouts/AppLayout";
 </script>
 
 <template>
-  <app-header />
-
-  <main class="w-full h-screen overflow-scroll bg-dark">
-    <RouterView />
-  </main>
-  <app-footer />
+  <AppLayout />
 </template>
---
+++ src/layouts/AppLayout/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app-layout">
+    <app-header />
+
+    <main class="w-full h-screen overflow-scroll bg-dark">
+      <RouterView />
+    </main>
+    <app-footer />
+  </div>
+</template>
+
+<script setup>
+import { AppFooter, AppHeader } from "@/layouts/AppLayout";
+</script>
+
+<style></style>
